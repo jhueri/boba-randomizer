3b2de1725d4f1b33749be430397b6d32ca7d2b77
Felicity Tabia <felicitygerielle@gmail.com>
2024-12-28 18:13:20 -0500

    Create boba.py

---
+++ boba.py
@@ -0,0 +1,6 @@
+from random import choice
+
+boba = ["Green Tea", "Black Tea", "Oolong Tea", "Earl Grey Tea", "Wintermelon", 
+        "Dirty Brown Sugar Milk Tea", "Creme Brulee Strawberry Latte"]
+
+print(choice(boba))
