009646e2eb9547b6a9fe0c1bc9d45cb27a536cbb
Felicity Tabia <felicitygerielle@gmail.com>
2024-12-28 18:20:00 -0500

    Update boba.py

--- boba.py
+++ boba.py
@@ -1,6 +1,11 @@
 from random import choice
 
-boba = ["Green Tea", "Black Tea", "Oolong Tea", "Earl Grey Tea", "Wintermelon", 
-        "Dirty Brown Sugar Milk Tea", "Creme Brulee Strawberry Latte"]
+boba = [["Green Tea", "Milk Foam Series", "Under 300 Calories"], ["Wintermelon", "Milk Foam Series", "Under 500 Calories"],
+        ["Caramel Milk Tea", "Milk Tea Series", "Under 500 Calories"]]
 
-print(choice(boba))
+print("Select from the Milk Foam Series or the Milk Tea Series?")
+series = input()
+
+for item in boba:
+    if item[1] == series:
+        print (series + " boba: " + item[0])
